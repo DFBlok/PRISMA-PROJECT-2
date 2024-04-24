6b6ac688f043e18a76f032c0e0b568882b5b8714
DFBlok <s219392862@mandela.ac.za>
2024-04-24 12:11:01 +0200

    zod validation

--- proj-2/app/api/issues/new/route.ts
+++ proj-2/app/api/issues/new/route.ts
@@ -1,13 +1,21 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/prisma/client";
+import { z } from "zod";
 
 //api/issues/new
+
+const IssueFromSchema = z.object({
+  title: z.string().min(1, { message: "This is required" }).max(50),
+  description: z.string().min(6, { message: "This is Description" }).max(200),
+});
+
 export async function POST(request: NextRequest) {
   const body = await request.json();
-  if (!body) {
-    return NextResponse.json({ error: "No data found" }, { status: 400 });
+  const validation = IssueFromSchema.safeParse(body);
+  if (!validation.success) {
+    return NextResponse.json(validation.error.format(), { status: 400 });
   }
-  const issue = prisma.issue.create({
+  const issue = await prisma.issue.create({
     data: {
       title: body.title,
       description: body.description,
