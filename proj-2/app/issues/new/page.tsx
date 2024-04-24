a014b43e21b7e2d84f8a53502c9603f0e1e1696a
DFBlok <s219392862@mandela.ac.za>
2024-04-24 14:46:50 +0200

    add simple MDE editor

--- proj-2/app/issues/new/page.tsx
+++ proj-2/app/issues/new/page.tsx
@@ -1,10 +1,14 @@
+"use client";
 import React from "react";
-import { TextField, TextArea, Button } from "@radix-ui/themes";
+import { TextField, Button } from "@radix-ui/themes";
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
+
 const NewIssue = () => {
   return (
     <div className="max-w-lg space-y-4 mt-5">
       <TextField.Root placeholder="This is Title..." />
-      <TextArea placeholder="Reply to comment..." />
+      <SimpleMDE placeholder="Reply to comment..." />
       <Button>Submit Issue</Button>
     </div>
   );
