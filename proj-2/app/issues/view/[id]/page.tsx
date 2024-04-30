a56763d6463c0e35bc12963811a300fdbddad8f3
DFBlok <s219392862@mandela.ac.za>
2024-04-30 10:03:44 +0200

    styling the issue page

--- proj-2/app/issues/view/[id]/page.tsx
+++ proj-2/app/issues/view/[id]/page.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import prisma from "@/prisma/client";
 import { notFound } from "next/navigation";
+import { Heading, Flex, Box } from "@radix-ui/themes";
+import IssueBadge from "@/app/components/IssueBadge";
 interface Props {
   params: { id: string };
 }
@@ -8,12 +10,17 @@ const IssueDetails = async ({ params }: { params: { id: string } }) => {
   const issue = await prisma.issue.findUnique({ where: { id: params.id } });
   if (!issue) notFound();
   return (
-    <div>
-      <p>{issue.title}</p>
-      <p>{issue.status}</p>
+    <Box as="div" className="space-y-4 mt-4">
+      <Heading as="h1" weight="medium">
+        {issue.title}
+      </Heading>
+      <Flex className="space-x-4">
+        <IssueBadge status={issue.status} />
+        <p>{issue.createdAt.toDateString()}</p>
+      </Flex>
+
       <p>{issue.description}</p>
-      <p>{issue.createdAt.toDateString()}</p>
-    </div>
+    </Box>
   );
 };
 
