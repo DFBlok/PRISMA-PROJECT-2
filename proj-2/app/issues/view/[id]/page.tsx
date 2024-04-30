6e8899611d2e2615a841f7f676e774a00c43bfdd
DFBlok <s219392862@mandela.ac.za>
2024-04-30 14:35:36 +0200

    added edit button

--- proj-2/app/issues/view/[id]/page.tsx
+++ proj-2/app/issues/view/[id]/page.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import prisma from "@/prisma/client";
 import { notFound } from "next/navigation";
-import { Heading, Flex, Box } from "@radix-ui/themes";
+import { Heading, Flex, Box, Card, Button, Grid } from "@radix-ui/themes";
 import IssueBadge from "@/app/components/IssueBadge";
 import Markdown from "react-markdown";
+import Link from "next/link";
 interface Props {
   params: { id: string };
 }
@@ -11,19 +12,31 @@ const IssueDetails = async ({ params }: { params: { id: string } }) => {
   const issue = await prisma.issue.findUnique({ where: { id: params.id } });
   if (!issue) notFound();
   return (
-    <Box as="div" className="space-y-4 mt-4">
-      <Heading as="h1" weight="medium">
-        {issue.title}
-      </Heading>
-      <Flex className="space-x-4">
-        <IssueBadge status={issue.status} />
-        <p>{issue.createdAt.toDateString()}</p>
-      </Flex>
-      <Box className="prose">
-        {" "}
-        <Markdown>{issue.description}</Markdown>
+    <Grid
+      columns={{ initial: "1", sm: "2" }}
+      gap="5"
+      justify="center"
+      align="center"
+    >
+      <Box as="div" className="space-y-4 mt-4">
+        <Heading as="h1" weight="medium">
+          {issue.title}
+        </Heading>
+        <Flex className="space-x-4">
+          <IssueBadge status={issue.status} />
+          <p>{issue.createdAt.toDateString()}</p>
+        </Flex>
+        <Card className="prose">
+          {" "}
+          <Markdown>{issue.description}</Markdown>
+        </Card>
       </Box>
-    </Box>
+      <Box>
+        <Button>
+          <Link href={`/issues/view/${issue.id}/edit`}>Update Issue</Link>
+        </Button>
+      </Box>
+    </Grid>
   );
 };
 
