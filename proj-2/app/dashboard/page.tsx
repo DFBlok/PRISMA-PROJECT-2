815d2d7f5abe119b0092bb5c42b241c0ddd34587
DFBlok <s219392862@mandela.ac.za>
2024-05-20 10:03:33 +0200

    dashboard added

--- proj-2/app/dashboard/page.tsx
+++ proj-2/app/dashboard/page.tsx
@@ -1,10 +1,25 @@
 import React from "react";
 import Paginition from "../issues/view/_components/Paginition";
-const Dashboard = () => {
+import prisma from "@/prisma/client";
+import IssueSummary from "./_components/IssueSummary";
+import LatestIssue from "./_components/LatestIssue";
+import IssueChart from "./_components/IssueChart";
+import { Grid, Flex } from "@radix-ui/themes";
+
+const Dashboard = async () => {
+  const open = await prisma.issue.count({ where: { status: "OPEN" } });
+  const inProgress = await prisma.issue.count({
+    where: { status: "IN_PROGRESS" },
+  });
+  const closed = await prisma.issue.count({ where: { status: "CLOSED" } });
   return (
-    <>
-      <Paginition itemCount={100} pageSize={10} currentPage={2} />
-    </>
+    <Grid columns={{ initial: "1", md: "2" }} gap="5">
+      <Flex direction="column" gap="5">
+        <IssueSummary open={open} inProgress={inProgress} closed={closed} />
+        <IssueChart open={open} inProgress={inProgress} closed={closed} />
+        <LatestIssue />
+      </Flex>
+    </Grid>
   );
 };
 
